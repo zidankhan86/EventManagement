2c6b1b37cfed7a1f831ec374c51a55d23a210bca
zidankhan86 <zidankhan718@gmail.com>
2023-10-12 19:56:00 +0600

    Features

--- resources/views/admin/pages/dashboard/dashboard.blade.php
+++ resources/views/admin/pages/dashboard/dashboard.blade.php
@@ -18,33 +18,29 @@
                       <div class="card card-mini mb-4">
                         <div class="card-body">
                           <h2 class="mb-1">71,503</h2>
-                          <p>Online Signups</p>
-                          <div class="chartjs-wrapper">
-                            <canvas id="barChart"></canvas>
-                          </div>
+                          <p>Total Events</p>
+
                         </div>
+                        <a href="" class="btn btn-dark">Details</a>
                       </div>
                     </div>
                     <div class="col-xl-3 col-sm-6">
                       <div class="card card-mini  mb-4">
                         <div class="card-body">
                           <h2 class="mb-1">9,503</h2>
-                          <p>New Visitors Today</p>
-                          <div class="chartjs-wrapper">
-                            <canvas id="dual-line"></canvas>
-                          </div>
+                          <p>Total Register Customer</p>
                         </div>
+                        <a href="" class="btn btn-dark">Details</a>
                       </div>
                     </div>
                     <div class="col-xl-3 col-sm-6">
                       <div class="card card-mini mb-4">
                         <div class="card-body">
                           <h2 class="mb-1">71,503</h2>
-                          <p>Monthly Total Order</p>
-                          <div class="chartjs-wrapper">
-                            <canvas id="area-chart"></canvas>
-                          </div>
+                          <p>Total Event Booked</p>
+
                         </div>
+                        <a href="" class="btn btn-dark">Details</a>
                       </div>
                     </div>
                     <div class="col-xl-3 col-sm-6">
@@ -52,10 +48,8 @@
                         <div class="card-body">
                           <h2 class="mb-1">9,503</h2>
                           <p>Total Revenue This Year</p>
-                          <div class="chartjs-wrapper">
-                            <canvas id="line"></canvas>
-                          </div>
                         </div>
+                        <a href="" class="btn btn-dark">Details</a>
                       </div>
                     </div>
                   </div>
