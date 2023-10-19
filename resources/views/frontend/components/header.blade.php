ed94a6ef8b9815813590d48937c8389b7982d88c
zidankhan86 <zidankhan718@gmail.com>
2023-10-20 00:50:25 +0600

    Logic for auth user

--- resources/views/frontend/components/header.blade.php
+++ resources/views/frontend/components/header.blade.php
@@ -9,10 +9,17 @@
         <div> <ul> <a class="nav-list" href="#">Create-events</a> </ul> </div>
         <div> <ul> <a class="nav-list" href="{{ url('/allEvent-page') }}">Contact</a> </ul> </div>
         <div> <ul> <a class="nav-list" href="#">About Us</a> </ul> </div>
+
+        @auth
+@if (auth()->user()->role == 'attendee' or auth()->user()->role == 'organizer')
+
+
         <div> <ul> <a class="nav-list" href="{{ url('/profile') }}"> <Strong style="color: green">Profile</Strong> </a> </ul> </div>
+        <div> <ul> <a class="nav-list" href="{{ url('/logout') }}"> <Strong style="color: rgb(216, 11, 11)">Logout</Strong> </a> </ul> </div>
 
+        @endauth
 
-
+        @else
         <div class="nav-button-background">
             <a href="{{ url('/login') }}" class="nav-login">Login</a>
         </div>
@@ -20,6 +27,9 @@
         <div class="nav-button-background">
             <a href="{{ url('/registration') }}" class="nav-signup">Signup</a>
         </div>
+
+        @endif
+
 
     </div>
     </div>
